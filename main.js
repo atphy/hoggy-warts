a9b4aa370fc5b25dde950e47065402f5b8a025f9
Austin Phy <atphy42@gmail.com>
2020-05-02 12:01:26 -0500

    added domString and example array

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+const students = [
+  {
+    id: 1,
+    name: 'Harry Potter',
+    school: 'Gryffindor',
+  },
+  {
+    id: 2,
+    name: 'Gandalf',
+    school: 'Ravenclaw'
+  },
+  {
+    id: 3,
+    name: 'Merlin',
+    school: 'HufflePuff'
+  },
+  {
+    id: 4,
+    name: 'Jafar',
+    school: 'Slytherin'
+  }
+]
+
+const printToDom = (selector, textToPrint) => {
+  const selectedDiv = document.querySelector(selector);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const buildStudents = (studentCollection) => {
+  domString = '';
+  for (let i = 0; i < studentCollection.length; i++) { 
+
+  domString += `
+  <div class="card">
+  <div class="card-header">
+    ${studentCollection[i].school}
+  </div>
+  <div class="card-body">
+    <h4 class="card-title">${studentCollection[i].name}</h4>
+    <a href="#" class="btn btn-primary">Expel</a>
+  </div>
+</div>
+  `
+  }
+  printToDom('#cardSection', domString);
+}
+
+
 const enterSite = () => {
   var enterWindow = document.getElementById("confirmPage");
     enterWindow.style.display = "none";
@@ -10,7 +58,8 @@ const clickConfirm = () => {
 
 const init = () => {
 document.getElementById("mainPage").style.display = "none";
-clickConfirm()
+clickConfirm();
+buildStudents(students)
 }
 
 init()
