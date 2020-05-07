b294b4f21d7c77d176b03b781068dadb43d9fe55
Austin Phy <atphy42@gmail.com>
2020-05-06 22:01:18 -0500

    Completed version of page. Added expel button functionality and created second array for expelled students.

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # hoggy-warts
-
-To-do:
-Add expel button functionality
-
-
-// BONUS //
-Add house colors to cards
-Sort cards by some criteria
-Create second array for expelled students
--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const printToDom = (selector, textToPrint) => {
 }
 
 const buildStudents = (studentCollection) => {
+  document.getElementById("studentsBox").hidden = false
   domString = '';
   for (let i = 0; i < studentCollection.length; i++) { 
 
@@ -26,12 +27,30 @@ const buildStudents = (studentCollection) => {
     </div>
     <div class="card-body">
       <h4 class="card-title">${studentCollection[i].name}</h4>
-      <a href="#" class="btn btn-primary" id="expelButton">Expel</a>
+      <button href="#" class="btn btn-primary expel" id="${studentCollection[i].id}">Expel</button>
     </div>
   </div>
   `
   }
   printToDom('#cardSection', domString);
+  clickExpel();
+}
+
+const buildExpelled = (expelledCollection) => {
+  document.getElementById("expelledBox").hidden = false
+  domString = '';
+  for (let i = 0; i < expelledCollection.length; i++) { 
+
+  domString += `
+    <div class="card" id="${expelledCollection[i].school}">
+    <div class="card-body">
+      <h4 class="card-title">${expelledCollection[i].name}</h4>
+      <img id="deathlyHallows" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Sign_of_the_Deathly_Hallows.svg/1179px-Sign_of_the_Deathly_Hallows.svg.png">
+    </div>
+  </div>
+  `
+  }
+  printToDom('#expelledSection', domString);
 }
 
 /* Removes introductory jumbotron on confirmation button click */
@@ -39,14 +58,17 @@ const enterSite = () => {
   var enterWindow = document.getElementById("confirmPage");
     enterWindow.style.display = "none";
   document.getElementById("mainPage").style.display = "block";
+  document.getElementById("studentsBox").hidden = true
+  document.getElementById("expelledBox").hidden = true
 }
 
-/* PROBLEM: Getting error in chrome console that addEventListener can't be added since #expelButton doesn't
-exist on page load.
-
+/* Sets up event listener for each individual expel button */
  const clickExpel = () => {
-  document.querySelector('#expelButton').addEventListener('click', expelStudent);
-} */
+  const expelBtns = document.getElementsByClassName('expel');
+    for (let i = 0; i < expelBtns.length; i++) {  
+    expelBtns[i].addEventListener('click', expelStudent);
+    };
+}
 
 const clickConfirm = () => {
   document.querySelector('#confirmButton').addEventListener('click', enterSite);
@@ -57,7 +79,7 @@ const clickSort = () => {
 }
 
 /* Defines variable that will be iterated over to assign each student a unique ID number */
-let assignId = 1
+let assignId = 0
 
 /* Creates new objects in the students array by pulling a student name from the text box,
  a school by randomly choosing a position in the schools array, and an ID by iterating over
@@ -83,27 +105,41 @@ if (document.getElementById("inputName").value === '') {
   alert("Incomplete paperwork. Please provide a student name before proceeding.");
   } else {
   sortStudent();
-  document.getElementById("inputName").value = ''
+  document.getElementById("inputName").value = '';
   }
 }
 
-/* const expelStudent = () => {
-  const expelledStudent = 
-  {
-    id: students.id,
-    name: students.name,
-    school: students.school
+/* Retrieves ID of clicked button, sets up an array listing student IDs in order, uses that array to find the 
+position of the student with an ID matching that of the button, removes it from the array of students and places it
+in an array for expelled students, then launches functions to rebuild cards from student array and to create cards for
+expelled students array.  */
+const expelStudent = (e) => {
+  const buttonId = e.target.id;
+  const studentIds = []
+  for (let i = 0; i < students.length; i++) {
+      studentIds.push(students[i].id.toString())
+      if (students[i].id === buttonId) {
+      }
   }
-  expelled.push(expelledStudent)
-  console.log(expelled)
-} */
+       const studentPosition = studentIds.indexOf(buttonId)
+       students[studentPosition].school = 'Voldemort'
+       expelled.push(students[studentPosition])
+       console.log('expelled', expelled)
+       students.splice(studentPosition, 1);
+       buildStudents(students)
+       buildExpelled(expelled)
+       console.log(students)
+       if (students.length === 0) {
+        document.getElementById("studentsBox").hidden = true
+       }
+}
 
 /* Defines init function, including setting everything but the jumbotron to hide on load */
 const init = () => {
   document.getElementById("mainPage").style.display = "none";
   clickConfirm();
   clickSort();
-  /*clickExpel();*/
+  /* clickExpel(); */
   buildStudents(students)
 }
 
